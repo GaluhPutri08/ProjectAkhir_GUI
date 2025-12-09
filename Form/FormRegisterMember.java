d8dbebb7c111933a4328211bd3cba4a7835d1770
GaluhPutri08 <galuh.saprilia@gmail.com>
2025-12-09 12:21:03 +0700

    penamaan class form reguster member

--- Form/FormRegisterMember.java
+++ Form/FormRegisterMember.java
@@ -5,7 +5,7 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
 import java.sql.*;
 
-public class FormRegistrasiMember {
+public class FormRegisterMember {
 
     public static void main(String[] args) {
 
