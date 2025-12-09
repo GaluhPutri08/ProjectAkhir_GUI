ff29b380c6d535bf7a6d771fc7f930ef74f6df38
Adhe Widya <adhewidya378@gmail.com>
2025-12-09 12:45:20 +0700

    Save local Koneksi.java

---
+++ Form/Koneksi.java
@@ -0,0 +1,23 @@
+package Form;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Koneksi {
+    private static Connection conn;
+
+    public static Connection getKoneksi() {
+        if (conn == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/gym_management";
+                String user = "root";
+                String pass = "";
+                conn = DriverManager.getConnection(url, user, pass);
+                System.out.println("Koneksi Berhasil");
+            } catch (Exception e) {
+                System.out.println("Koneksi Gagal: " + e.getMessage());
+            }
+        }
+        return conn;
+    }
+}
